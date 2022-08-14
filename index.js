ef9e84a4a06ab7d8b34037c661a23bbd4789706a
khafiluhur <khafiluhur.99@gmail.com>
2022-08-14 15:25:16 +0700

    javascript done

--- index.js
+++ index.js
@@ -1,81 +1,68 @@
-class Setting {
-  #bot;
-  constructor(player, bot) {
-    this.player = player;
-    this.#bot = bot;
+class Game {
+  constructor(player) {
+    this.player = player,
+    this._bot = ""
   }
-  #randomBot() {
-    let botPick = ["kertas", "gunting", "batu"];
-    let test = botPick[Math.floor(Math.random() * botPick.length)];
-    return this.#bot = test;
+
+  set bot(bot) {
+    this._bot = bot;
   }
-  publicBot() {
-    return this.#randomBot();
+
+  get bot() {
+    return this._bot;
   }
-  setPlayerPicK(player){
-    this.player = player;
+
+  setBot(bot){
+    this._bot = bot;
   }
-  getPlayerPicK(){
-    return this.player;
+
+  getBot(){
+    return this._bot;
+  }
+
+  #isRandom() {
+    let bot = ['paper', 'rock', 'scissors'];
+    return bot[Math.floor(Math.random() * bot.length)]; 
+  }
+
+  getIsRandom(){
+    return this.#isRandom();
+  }
+
+  #isRule() {
+    let player = this.player;
+    this.getBot();
+    let botPick = this._bot;
+
+    if(
+      (player === 'paper' && botPick === 'rock') || 
+      (player === "rock" && botPick === "scissors") || 
+      (player === "scissors" && botPick === "paper")) {
+        return "Player Win";
+    } else if (player === botPick) {
+      return "Game Draw";
+    } else {
+      return "Player Lose";
+    }
+  }
+
+  getIsRule(){
+    return this.#isRule();
   }
 }
 
-class gamePlay extends Setting {
-  constructor(player,bot,winner) {
-    super(player,bot);
-    this.winner = winner;
+class Play extends Game {
+  constructor(player) {
+    super(player);
   }
-  #rulesGame() {
-    // if (
-    //     (this.getPlayerPicK() == "kertas" && this.publicBot() == "batu") ||
-    //     (this.getPlayerPicK() == "batu" && this.publicBot() == "gunting") ||
-    //     (this.getPlayerPicK() == "gunting" && this.publicBot() == "kertas")
-    //   ) {
-    //     return this.winner = this.player
-    //   } else if (this.getPlayerPicK() === this.publicBot()) {
-    //     return this.winner = this.bot
-    //   } else {
-    //     this.winner = "DRAW"
-    //   }
-    return this.player;
-  }
-  publicRules() {
-    // console.log(this.#rulesGame())
-    return this.#rulesGame();
-  }
-  matchResult() {
-    return;
+
+  play() {
+    return this.getIsRule();
   }
 }
 
-let start = new gamePlay();
-let setting = new Setting();
-setting.setPlayerPicK('kertas');
-console.log(setting.getPlayerPicK());
-
-// console.log(start.randomBot());
-// console.log(start.playerPick());
-// console.log(start.rulesGame());
-// console.log(start.matchResult());
-
-console.info(start.publicBot());
-console.info(start.publicRules());
-
-
-// document.addEventListener("click",pilihanPlayer);
-// function pilihanPlayer() {
-//   let player = document.getElementsByTagName("player");
-//   for (let i = 0; i < player.length; i++) {
-//     // for (let j = 0; j < player[i].length; j++) {
-//     //   player[i][j].addEventListener('click', function(){
-//     //     console.log(player[0].alt);
-//     //   }, false);
-//     // }
-//     console.log(player[0]);
-//     // const element = player;
-//     // element[0] = "satu"
-//     // element[1] = "dua"
-//     // element[3] = "tiga"
-//   }
-//   // console.log(player);
-// }
+let game = new Play();
+game.player = 'paper';
+game.setBot(game.getIsRandom());
+console.log("Computer " + game.getBot(), "Player " + game.player)
+console.log(game.getIsRule())
